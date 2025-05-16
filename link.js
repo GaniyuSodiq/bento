9a5bb0b33d991e6e5cba48b887cc9398c6d19d42
GaniyuSodiq <82044466+GaniyuSodiq@users.noreply.github.com>
2025-05-16 06:47:10 +0100

    Catch the error if the URL is invalid.
    Check is the URL data is valid
    else make the bento container display invalid URL

--- link.js
+++ link.js
@@ -1,6 +1,17 @@
 function main(){
     const bentoContainer = document.getElementById("bento-container");
+    // GET THE CONTENT OF THE ADDRESS BAR
+    // SPLIT IT INTO AN ARRAY TO VALUES WITH "?data=" THAT'S IN IT
+    // GET THE SECOND VALUE IN THE ARRAY
     const data = window.location.href.split("?data=")[1];
+
+    let bentoData;
+    try {
+        bentoData = JSON.parse(decodeURIComponent(window.atob(data)));
+    } catch (error) {
+        bentoContainer.innerText = "You entered an invalid URL";
+        return;
+    }
 }
 
 main()

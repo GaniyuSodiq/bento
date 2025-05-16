81cbab6b32a1c46d74232ba805f7a51dfd12f6d0
GaniyuSodiq <82044466+GaniyuSodiq@users.noreply.github.com>
2025-05-16 07:14:57 +0100

    Save the bentometadata content into elements

--- link.js
+++ link.js
@@ -7,11 +7,30 @@ function main(){
 
     let bentoData;
     try {
+        // CONVERT THE DATA BACK TO AN ARRAY AND STORE THE ARRAY INTO BENTODATA
         bentoData = JSON.parse(decodeURIComponent(window.atob(data)));
     } catch (error) {
+        // IF BENTODATA GETS AN ERROR, THEN RUN THE CODE HERE
         bentoContainer.innerText = "You entered an invalid URL";
         return;
     }
+    // POP OUT THE FIRST PART OF THE BENTODATA USING SHIFT() AND SAVE INTO BENTOMETADATA
+    const bentoMetaData = bentoData.shift();
+
+    // NOW WE CREATE THE ELEMENT INWHICH WE ARE GOING TO DISPLAY THE DATA FROM BENTOMETADATA
+    const bento = {
+        title : document.createElement("h1"),
+        author : document.createElement("h3"),
+        description : document.createElement("p"),
+        linkContainer : document.createElement("div")
+    }
+
+    // NOW PUT THE BENTOMETADATA INTO THE ELEMENTS WE CREATED
+    bento.title.innerText = bentoMetaData.bentoTitle;
+    bento.author.innerText = "Author: " + bentoMetaData.bentoAuthor;
+    bento.description.innerText = bentoMetaData.bentoDescription;
+
+
 }
 
 main()

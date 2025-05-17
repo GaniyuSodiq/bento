a49817bf8e1e47df693179c3adb9fb09a0717d75
GaniyuSodiq <82044466+GaniyuSodiq@users.noreply.github.com>
2025-05-17 21:29:27 +0100

    Complete the link creation part

--- redo/script.js
+++ redo/script.js
@@ -1,3 +1,4 @@
+createBasketLink()
 const addMoreLink = document.getElementById("add-more-link-btn");
 addMoreLink.addEventListener("click", () => {
     createBasketLink()
@@ -7,10 +8,14 @@ function createBasketLink () {
     const root = {
         linkContainer : document.createElement("div"),
         link : document.createElement("input"),
+        linkTtile : document.createElement("input"),
+        linkDescription : document.createElement("input"),
         removeLinkButton : document.createElement("button"),
     };
 
     root.linkContainer.appendChild(root.link);
+    root.linkContainer.appendChild(root.linkTtile);
+    root.linkContainer.appendChild(root.linkDescription)
     root.linkContainer.appendChild(root.removeLinkButton);
 
     const basketOfLinks = document.querySelector("#basket");
@@ -20,6 +25,16 @@ function createBasketLink () {
         e.preventDefault();
         root.linkContainer.remove();
     })
+
+    root.removeLinkButton.textContent = "Remove Link"
+    root.linkContainer.classList.add("link")
+    root.link.placeholder = "Enter Your Link Here"
+    root.link.name = "link"
+    root.link.type = "url"
+    root.linkDescription.placeholder = "Enter Your Link Description Here"
+    root.linkDescription.name = "link-description"
+    root.linkTtile.placeholder = "Enter Your Link Title Here"
+    root.linkTtile.name = "link-title"
 }
 
 

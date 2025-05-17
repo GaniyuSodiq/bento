3c721257920b5251efa65444da051e04f8ca6de2
GaniyuSodiq <82044466+GaniyuSodiq@users.noreply.github.com>
2025-05-17 18:59:32 +0100

    for some weird reason i still cant fix remove error

--- redo/script.js
+++ redo/script.js
@@ -1,34 +1,10 @@
+const addMoreLink = document.getElementById("add-more-link-btn");
+addMoreLink.addEventListener("click", () => {
+    createBasketLink()
+})
+
 function createBasketLink () {
-
-    const basketOfLinks = document.querySelector("#basket");
-
-    root = {
-        link : document.createElement("input"),
-        linkTitle : document.createElement("input"),
-        linkDescription : document.createElement("input"),
-        removeLinkButton : document.createElement("button"),
-        linkContainer : document.createElement("div"),        
-    }
-
-    root.linkContainer.classList.add("link")
-    root.link.type = "url";
-    root.link.placeholder = "Link";
-    root.linkTitle.placeholder = "Link Title";
-    root.linkDescription.placeholder = "Link Description";
-    root.removeLinkButton.innerText = "Remove This Link";
-    root.removeLinkButton.type = "button"
-    root.removeLinkButton.addEventListener("click", () => {
-        root.linkContainer.remove();
-    })
-
-    root.linkContainer.appendChild(root.link);
-    root.linkContainer.appendChild(root.linkTitle);
-    root.linkContainer.appendChild(root.linkDescription);
-    root.linkContainer.appendChild(root.removeLinkButton);
-
-    basketOfLinks.appendChild(root.linkContainer);
+    
 }
 
-createBasketLink();
-createBasketLink();
-createBasketLink();
+

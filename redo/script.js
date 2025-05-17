b52444c3664e080adee99cddf6aca57c29d0ebc4
GaniyuSodiq <82044466+GaniyuSodiq@users.noreply.github.com>
2025-05-17 21:10:41 +0100

    Finally got the remove button to work.
    This took me 7 hours
    
    Lesson: execute core ideas on very short code blocks before expanding
    the block

--- redo/script.js
+++ redo/script.js
@@ -4,7 +4,22 @@ addMoreLink.addEventListener("click", () => {
 })
 
 function createBasketLink () {
-    
+    const root = {
+        linkContainer : document.createElement("div"),
+        link : document.createElement("input"),
+        removeLinkButton : document.createElement("button"),
+    };
+
+    root.linkContainer.appendChild(root.link);
+    root.linkContainer.appendChild(root.removeLinkButton);
+
+    const basketOfLinks = document.querySelector("#basket");
+    basketOfLinks.appendChild(root.linkContainer);
+
+    root.removeLinkButton.addEventListener("click", (e)=>{
+        e.preventDefault();
+        root.linkContainer.remove();
+    })
 }
 
 

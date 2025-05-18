23cca88eecd17a983973464976d5fd71314ee5a2
GaniyuSodiq <82044466+GaniyuSodiq@users.noreply.github.com>
2025-05-18 14:27:38 +0100

    Style the basket details section of the link

--- redo/link.js
+++ redo/link.js
@@ -1,12 +1,20 @@
 const linkBasketDisplay = document.querySelector("#link-basket-display")
 
 const incomingData = window.location.href.split("?data=")[1]
-// const refinedData = JSON.parse(decodeURIComponent(window.atob(incomingData)))
 
+let basketData;
+// try {
+//     basketData = JSON.parse(decodeURIComponent(window.atob(incomingData)));
+//     console.log(basketData)
+// } catch (error) {
+//        linkBasketDisplay.classList.add("centered-text")
+//     linkBasketDisplay.innerText = "The URL is invalid!"
+// }
 
-try {
-    let basketData = JSON.parse(decodeURIComponent(window.atob(incomingData)));
-    console.log(basketData)
-} catch (error) {
-    linkBasketDisplay.textContent = "The URL is invalid!"
-}
+const basketDetails = basketData.shift()
+
+const basketTitle = document.createElement("h1")
+const basketDescription = document.createElement("h2")
+const basketAuthor = document.createElement("h3")
+const basketDetailsContainer = document.createElement("h1")
+

4f4ff43eb5d14890b7c3c6a7bd39ac732fbcc175
GaniyuSodiq <82044466+GaniyuSodiq@users.noreply.github.com>
2025-05-19 07:29:56 +0100

    Add Array of objects that contains all the color and deatils

---
+++ color-teacher/script.js
@@ -0,0 +1,17 @@
+const colorArray = [
+    {
+        name : "RED",
+        code : "RED",
+        audio : null,
+    },
+        {
+        name : "GREEN",
+        code : "GREEN",
+        audio : null,
+    },
+        {
+        name : "WHITE",
+        code : "WHITE",
+        audio : null,
+    },
+]

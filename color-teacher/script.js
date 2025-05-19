1c638cce22c2859a55852a28cc641ad0fcf4bc8c
GaniyuSodiq <82044466+GaniyuSodiq@users.noreply.github.com>
2025-05-19 07:45:35 +0100

    CRACK HOW TO PICK RANDOM COLOR FROM THE ARRAY

--- color-teacher/script.js
+++ color-teacher/script.js
@@ -4,14 +4,17 @@ const colorArray = [
         code : "RED",
         audio : null,
     },
-        {
+    {
         name : "GREEN",
         code : "GREEN",
         audio : null,
     },
-        {
+    {
         name : "WHITE",
         code : "WHITE",
         audio : null,
     },
 ]
+
+let randomColorSelection = colorArray[Math.floor(Math.random()*colorArray.length)].name;
+console.log(randomColorSelection);
